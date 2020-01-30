3f662bb2685e74c19112680a22c86256b0bc66d5
Pavel Beletsky <pavel.beletsky@live.com>
2020-01-30 17:05:32 -0500

    request tabs

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,11 +1,47 @@
 import React, { Fragment } from 'react'
+import MaterialTable from 'material-table'
+
+import tableIcons from './MTcomponents'
+
 
 const Auth = (props) => {
-
 
   return (
     <Fragment>
       <h4>Authorization</h4>
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          icons={tableIcons}
+          title="Authorization"
+          // totalCount={1}
+          options={{
+            search: false,
+            paging: false
+          }}
+          columns={[
+            { title: 'Key', field: 'key' },
+            { title: 'Value', field: 'value', initialEditValue: 'initial edit value' },
+            { title: 'Add to', field: 'add_to' }
+          ]}
+          data={[
+            { key: 'API_KEY', value: 'QIU12NBV3128ASJH8', add_to: 'headers' }
+          ]}
+          editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    /* const data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data[index] = newData;                
+                    this.setState({ data }, () => resolve()); */
+                  }
+                  resolve();
+                }, 1000);
+              })
+          }}
+        />
+      </div>
     </Fragment>
   )
 }
--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,11 +1,78 @@
 import React, { Fragment } from 'react'
+import MaterialTable from 'material-table'
+
+import tableIcons from './MTcomponents'
+
 
 const Headers = (props) => {
-
 
   return (
     <Fragment>
       <h4>Headers</h4>
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          icons={tableIcons}
+          title="Headers table"
+          options={{
+            search: false,
+            paging: false
+          }}
+          columns={[
+            { title: 'Key', field: 'key' },
+            { title: 'Value', field: 'value', initialEditValue: 'initial edit value' },
+            { title: 'Description', field: 'description' }
+          ]}
+          data={[
+            { key: 'country', value: 'US', description: 'none' },
+            { key: 'city', value: 'New York', description: 'none' }
+          ]}
+          // options={{ selection: true }}
+          // actions={[
+          //   {
+          //     tooltip: 'Remove All Selected Users',
+          //     icon: 'delete',
+          //     onClick: (evt, data) => alert('You want to delete ' + data.length + ' rows')
+          //   }
+          // ]}
+          editable={{
+            onRowAdd: newData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    /* const data = this.state.data;
+                    data.push(newData);
+                    this.setState({ data }, () => resolve()); */
+                  }
+                  resolve();
+                }, 1000);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    /* const data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data[index] = newData;                
+                    this.setState({ data }, () => resolve()); */
+                  }
+                  resolve();
+                }, 1000);
+              }),
+            onRowDelete: oldData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    /* let data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data.splice(index, 1);
+                    this.setState({ data }, () => resolve()); */
+                  }
+                  resolve();
+                }, 1000);
+              })
+          }}
+        />
+      </div>
     </Fragment>
   )
 }
